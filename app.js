65d6bfc3d9e463a4fbc7fd4276bbb0b9aaa8500e
root <root@localhost.localdomain>
2017-06-06 10:00:22 +0100

    added ssl keys

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var db = mongoose.connect("mongodb://localhost/quench");
 
 //ssl
 var sslOptions = {
-  key: fs.readFileSync('key.pem'),
+  key: fs.readFileSync('privkey.pem'),
   cert: fs.readFileSync('cert.pem'),
   passphrase: 'admin123@pass'
 };
