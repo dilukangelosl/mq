83849a6f73b16e2343ba48fe1364b406655ba084
diluk Aneglo <Dilukangelo@gmail.com>
2017-06-06 13:56:07 +0530

    sample ssl

--- app.js
+++ app.js
@@ -4,13 +4,22 @@ var cors = require('cors')
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var path = require('path');
+var https = require('https');
 //routes
 var userRouter = require('./routes/users');
 var imageRouter = require('./routes/images');
 var retailerRoute = require('./routes/retailer');
-
+var fs = require('fs');
 //connect to db
 var db = mongoose.connect("mongodb://localhost/quench");
+
+//ssl
+var sslOptions = {
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem'),
+  passphrase: 'admin123@pass'
+};
+
 
 //app uses
 app.use(cors())
@@ -18,11 +27,18 @@ app.use('/uploads', express.static(__dirname + '/uploads'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+/*
 app.listen(8000, function(){
     console.log("server is running on 8000");
-})
+})*/
+
 
 
 app.use('/users', userRouter);
 app.use('/images', imageRouter);
 app.use('/retailers', retailerRoute);
+
+https.createServer(sslOptions, app).listen(8443, function(){
+ console.log("server is running on secured port 8443");
+
+})
