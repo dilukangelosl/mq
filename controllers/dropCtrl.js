11e098b02a5c1111994651bb4a24c4dcccc72c5d
diluk Aneglo <Dilukangelo@gmail.com>
2017-07-19 18:17:09 +0530

    xx

--- controllers/dropCtrl.js
+++ controllers/dropCtrl.js
@@ -24,65 +24,65 @@ var get = function (req, res) {
 
 var getusersbyretailerId = function (req, res) {
 
-    Drop.find({ "retailerid": req.params.id, "redeemed": false }, function (err, data) {
-        //needed var 
-        var newdata = [];
-        var startingcounter = 0;
-        var counter = {};
-        if (err) {
-            res.status(500);
-            res.send("Cannot find by Id");
+  Drops.find({ "retailerid": req.params.id, "redeemed": false }, function (err, data) {
+    //needed var 
+    var newdata = [];
+    var startingcounter = 0;
+    var counter = {};
+    if (err) {
+        res.status(500);
+        res.send("Cannot find by Id");
+    }
+
+    else {
+
+
+        for (var i = 0; i < data.length; i += 1) {
+            counter[data[i].userid] = (counter[data[i].userid] || 0) + 1;
+
+            if (i == data.length - 1) {
+
+                //do rest 
+                var objlength = Object.keys(counter).length;
+
+                finduseranddotherest(objlength);
+
+            }
         }
 
-        else {
+    }
 
+    //nested functions
 
-            for (var i = 0; i < data.length; i += 1) {
-                counter[data[i].userid] = (counter[data[i].userid] || 0) + 1;
+    function finduseranddotherest(countersize) {
+        var keyoftheobject = Object.keys(counter)[startingcounter];
+        console.log("user Key  == " + keyoftheobject + " keydata ==" + counter[keyoftheobject]);
 
-                if (i == data.length - 1) {
+        User.find({ "authId": keyoftheobject }, function (err, data) {
 
-                    //do rest 
-                    var objlength = Object.keys(counter).length;
+            if (err) {
+                res.status = 200;
+                res.send(err);
+            }
+            else {
+                newdata.push({ userid: key, name: data[0].firstname + " " + data[0].lastname, dob: data[0].dob, gender: data[0].gender, email: data[0].email, drops: counter[keyoftheobject] })
+                if (startingcounter == countersize) {
 
-                    finduseranddotherest(objlength);
+                    res.status = 200;
+                    res.send(newdata);
 
+                } else {
+                    console.log("we have " + keyoftheobject + " duplicated " + counter[keyoftheobject] + " times");
+                    startingcounter++;
+                    finduseranddotherest(countersize);
                 }
             }
 
-        }
+        });
 
-        //nested functions
+    }
 
-        function finduseranddotherest(countersize) {
-            var keyoftheobject = Object.keys(counter)[startingcounter];
-            console.log("user Key  == " + keyoftheobject + " keydata ==" + counter[keyoftheobject]);
-
-            User.find({ "authId": keyoftheobject }, function (err, data) {
-
-                if (err) {
-                    res.status = 200;
-                    res.send(err);
-                }
-                else {
-                    newdata.push({ userid: key, name: data[0].firstname + " " + data[0].lastname, dob: data[0].dob, gender: data[0].gender, email: data[0].email, drops: counter[keyoftheobject] })
-                    if (startingcounter == countersize) {
-
-                        res.status = 200;
-                        res.send(newdata);
-
-                    } else {
-                        console.log("we have " + keyoftheobject + " duplicated " + counter[keyoftheobject] + " times");
-                        startingcounter++;
-                        finduseranddotherest(countersize);
-                    }
-                }
-
-            });
-
-        }
-
-    });
+});
 
 
 
